5ae44c41d747beb0cec34f24e1db5b87efdfe9c0
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-08-17 13:33:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  if (number === 0) {
+    return 'zero';
+  }
+
+  function convertToWords(num) {
+    if (num < 10) {
+      return ones[num];
+    }
+
+    if (num < 20) {
+      return teens[num - 10];
+    }
+
+    if (num < 100) {
+      const remainder = num % 10;
+      return `${tens[Math.floor(num / 10)]}${remainder !== 0 ? ` ${ones[remainder]}` : ''}`;
+    }
+
+    if (num < 1000) {
+      const hundreds = Math.floor(num / 100);
+      const remainder = num % 100;
+      const hundredsWord = `${ones[hundreds]} hundred`;
+
+      if (remainder === 0) {
+        return hundredsWord;
+      }
+      return `${hundredsWord} ${convertToWords(remainder)}`;
+    }
+
+    return '';
+  }
+
+  return convertToWords(number);
 };
